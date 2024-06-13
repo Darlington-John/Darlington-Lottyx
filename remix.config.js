387137a3bb62f521bebad238aed6bc74b9350534
Darlington-John <onuohadarlington38@gmail.com>
2024-06-13 09:51:57 +0100

    works

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/*.css"],
+  server: {
+    secrets: ["CLERK_SECRET_KEY"],
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
