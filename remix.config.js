7932c543ee80e0d054450c0c62aa87fd5ef6ea89
Darlington-John <onuohadarlington38@gmail.com>
2024-06-13 09:54:31 +0100

    hhhb hjb

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/*.css"],
-  server: {
-    secrets: ["CLERK_SECRET_KEY"],
-  },
+
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
