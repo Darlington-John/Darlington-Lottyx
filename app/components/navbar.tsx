770e99a748340ddc91fa95f60dc615e63934cd3c
Darlington-John <onuohadarlington38@gmail.com>
2024-06-18 15:08:10 +0100

    don't be afraid of me

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,17 +4,14 @@ import coinImg from './../assets/icons/coin.png'
 import BellImg from './../assets/icons/Bell.png'
 import plusImg from './../assets/icons/plus.png'
 import useForm from "./hooks/useForm";
-import { UserButton } from "@clerk/remix";
 const Navbar = ({user}) => {
 
 
     return (  <div className="flex  items-center justify-between sticky w-full top-0 z-40 text-white text-sm px-5  py-3 ">
         <div className="flex items-center gap-2">
-        <UserButton/>
 <h1 >
     Hello {user.name}
 </h1>
-
 </div>
 <div className="flex gap-2">
     <Button  label={true} labelImg={coinImg} labelText="124,000" bg="#00000040" icon={true} img={plusImg} />
